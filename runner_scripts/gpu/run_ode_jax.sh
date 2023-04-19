e6d176fcaae8966f4f100dd41c8877f335a6220c
Utkarsh <rajpututkarsh530@gmail.com>
2023-04-19 15:56:59 -0400

    Add jax ode scripts

---
+++ runner_scripts/gpu/run_ode_jax.sh
@@ -0,0 +1,11 @@
+a=8
+max_a=$1
+XLA_PYTHON_CLIENT_PREALLOCATE=false
+while [ $a -le $max_a ]
+do
+    	# Print the values
+    	echo "No. of trajectories = $a"
+		python3 ./GPU_ODE_JAX/benchmark_diffrax.py $a	
+    	# increment the value
+    	a=$((a*4))
+done
