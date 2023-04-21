7794d0ca017eeff91d366b238a3daaeea9b33eff
Utkarsh <rajpututkarsh530@gmail.com>
2023-04-20 23:05:01 -0400

    Update test scipt

--- test_DiffEqGPU.jl
+++ test_DiffEqGPU.jl
@@ -6,5 +6,8 @@ TestEnv.activate("DiffEqGPU")
 backend = ARGS[1]
 ENV["GROUP"] = backend
 Pkg.add(backend)
+
+ENV["JULIA_LOAD_PATH"]=dirname(Base.active_project())
+
 using DiffEqGPU
 include(joinpath(dirname(pathof(DiffEqGPU)), "..", "test", "runtests.jl"))
